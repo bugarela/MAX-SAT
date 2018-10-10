2ee9cf54ebd0533d25ccc53519d25f39f4e04cc5
Gabriela Moreira <gabriela@magrathealabs.com>
2018-10-10 11:40:10 -0300

    Random Search

--- maxsat.py
+++ maxsat.py
@@ -1,55 +1,109 @@
-import random
+from random import randint
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 filename = 'uf20-01.cnf'
+max_iterations = 250000
 
 with open(filename,'r') as file:
      all_lines = file.readlines()
 
 
 def initial_solution(n_vars):
-    solution = np.zeros(n_vars)
-    for variable in solution:
-        variable = randint(0, 1)
+    solution = []
+    for _ in range(n_vars):
+        solution.append(randint(0, 1))
     return solution
 
 
-def to_tuple(n):
-    n = n - 1
+def to_tuple(n_str):
+    n = int(n_str)
     if n < 0:
-        return (-1*n, 0)
+        return (-1*(n+1), 0)
     else:
-        return (n, 1)
+        return (n-1, 1)
 
 
 def evaluate(clause, solution):
     sucesses = []
     for position, value in clause:
         sucesses.append(solution[position] == value)
-    return sucesses.any()
+    return any(sucesses)
 
 
+def evaluate_all(clauses, solution):
+    count = 0
+    for clause in clauses:
+        if evaluate(clause, solution):
+            count += 1
+    return count
+
+
+def random_search(clauses, solution):
+    new_solution = solution
+    score = evaluate_all(clauses, solution)
+    iterations = 0
+
+    scores = []
+
+    while iterations < max_iterations:
+        iterations += 1
+        disturbance = randint(0, len(solution)-1)
+        new_solution[disturbance] = 1 - solution[disturbance]
+        new_score = evaluate_all(clauses, new_solution)
+        if new_score >= score:
+            solution = new_solution
+            score = new_score
+        else:
+            new_solution = solution
+        scores.append(score)
+    print(len(np.arange(max_iterations)), len(scores))
+    sns.lineplot(x=np.arange(max_iterations), y=scores)
+    plt.savefig('random_search_convergence.png')
+
+    return solution, score
+
+def simmulated_annealing(clauses, solution):
+    ## TO DO
+    new_solution = solution
+    score = evaluate_all(clauses, solution)
+    iterations = 0
+
+    scores = []
+
+    while iterations < max_iterations:
+        iterations += 1
+        disturbance = randint(0, len(solution)-1)
+        new_solution[disturbance] = 1 - solution[disturbance]
+        new_score = evaluate_all(clauses, new_solution)
+        if new_score >= score:
+            solution = new_solution
+            score = new_score
+        else:
+            new_solution = solution
+        scores.append(score)
+    print(len(np.arange(max_iterations)), len(scores))
+    sns.lineplot(x=np.arange(max_iterations), y=scores)
+    plt.savefig('simmulated_annealing_convergence.png')
+
+    return solution, score
+
+clauses = []
 for line in all_lines:
     if line.startswith('p'):
-        print(line)
-        a, b, n_vars, n_cla = line.split(' ')
-        solution = initial_solution(n_vars)
+        _, _, n_vars, n_cla = line.split()
+        n_vars = int(n_vars)
+        n_cla = int(n_cla)
 
-    clauses = []
-    if not line.startswith('c'):
-        v1, v2, v3, _ = line.split(' ')
-        clauses.append(to_tuple(v1), to_tuple(v2), to_tuple(v3))
+    elif line.startswith('%'):
+        break
 
-################ IDEAS ################
-# Avaliar solucao
-count = 0
-for clause in clauses:
-    if evaluate(clause, solution):
-        count += 1
+    elif not line.startswith('c'):
+        v1, v2, v3, _ = line.split()
+        clauses.append([to_tuple(v1), to_tuple(v2), to_tuple(v3)])
 
-# Perturbar solucao
-disturbance = randint(0, n_vars-1)
-solution[disturbance] = 1 - solution[disturbance]
-
-# clause = [(1,1), (2,0)]
-
-# solution = [1, 1, 1, 1, ...]
+solution, score = random_search(clauses, initial_solution(n_vars))
+print(solution, score)
